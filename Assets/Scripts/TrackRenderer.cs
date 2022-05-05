262033362b5f04d2fb6484d9c19842797f5d863b
Najtex <timi.kompi@gmail.com>
2022-05-05 23:23:08 +0200

    Fixed track rendering

--- Assets/Scripts/TrackRenderer.cs
+++ Assets/Scripts/TrackRenderer.cs
@@ -7,10 +7,17 @@ public class TrackRenderer : MonoBehaviour
     
     public static TrackRenderer instance;
 
+    string name = "Track01";
+
     void Awake()
     {
         instance = this;
-        LoadTrack("Track01");
+
+        PlayerPrefs.SetString("trackName", name);
+
+        string trackName = PlayerPrefs.GetString("trackName");
+
+        LoadTrack(trackName);
     }
 
     public void LoadTrack(string trackName)
@@ -18,7 +25,7 @@ public class TrackRenderer : MonoBehaviour
         string trackPath = "Tracks/" + trackName;
         
         GameObject track = (GameObject)Resources.Load(trackPath, typeof(GameObject));
-        track = Instantiate(track, transform.position, transform.rotation);
+        track = Instantiate(track, new Vector3(0, 0, 0), Quaternion.identity);
 
     }
 }
